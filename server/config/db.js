247e64fb6bd31e330c706ba300bf68f77f3cbbe8
Maria Albufera <malbufer@my.centennialcollege.ca>
2022-10-19 14:07:27 -0400

    Adding files

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store",
+    "URI":"mongodb+srv://namira:SWJBrKAd8AlSxvKV@databaseserver.9d4sb.mongodb.net/products_store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
